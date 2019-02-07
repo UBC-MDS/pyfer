c1b58703ac975f3dfbd3527ef18a57c71d769281
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2019-02-07 00:24:53 -0800

    created first dummy function

---
+++ functions.py
@@ -0,0 +1,13 @@
+def generate(df):
+    '''
+    Generate bootstrap resamples and permutations
+
+    Parameters:
+    data (pd.DataFrame): a Dataframe generated from a specify function.
+    n_samples (int): Number of resamples.
+    type (str): "Bootstrap" (default), or "Permutation".
+
+    Returns:
+    (pd.DataFrame): Dataframe containing columns for response, sample_id and, if specified, explanatory variables.
+    '''
+    pass
