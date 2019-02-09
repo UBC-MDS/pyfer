dc825158c746b5f3a244fa8581104a585fea2325
Gabriel Bogo <gabrielmedeirosbogo@gmail.com>
2019-02-08 17:27:52 -0800

    unified functions in a single file

--- pyfer/functions.py
+++ pyfer/functions.py
@@ -1,15 +1,78 @@
 import pandas as pd
+import numpy as np
+
+def specify(data, response):
+    '''
+    Choose specific columns to feed the subsequent pipeline.
+
+    Parameters:
+    ---------------
+    data: pd.DataFrame
+    response: string
+        One column of the dataframe to be the response variable.
+    explanatory: string or list of strings
+        Columns to be the explanatory variables
+
+    Returns:
+    ---------------
+    pd.DataFrame:
+        Dataframe containing one column for response variable and zero or more columns for the explanatory variables. The first column is always the response.
+    '''
+
+    return pd.DataFrame()
 
 def generate(data, n_samples, type="boostrap"):
     '''
     Generate bootstrap resamples and permutations
 
     Parameters:
-    data (pd.DataFrame): a Dataframe generated from a specify function.
-    n_samples (int): Number of resamples.
-    type (str): "Bootstrap" (default), or "Permutation".
+    ---------------
+    data: pd.DataFrame
+        A Dataframe containing a response column (the first one) and zero or more explanatory columns. Usually the output of a specify function.
+    n_samples: integer
+        Number of resamples.
+    type: string
+        "Bootstrap" (default), or "Permutation".
 
     Returns:
-    (pd.DataFrame): Dataframe containing columns for response, sample_id and, if specified, explanatory variables.
+    ---------------
+    pd.DataFrame:
+        Dataframe containing all resamples stacked vertically. Will keep all columns from the input data and an additional sample_id column to identify individual resamples.
+    '''
+    return pd.DataFrame()
+
+def calculate(data, stat="mean"):
+    '''
+    Calculate a summarizing statistic for each bootstrap sample.
+
+    Parameters:
+    ---------------
+    data: pd.DataFrame
+        A Dataframe generated from `generate` function with columns: response, sample_id and zero or more explanatory variables.
+    stat: string
+        "mean" (default) or "median".
+
+    Returns:
+    ---------------
+    pd.DataFrame:
+        Summarized data. Each row contains the summary statistic for a given resample.
+    '''
+    return pd.DataFrame()
+
+def get_ci(data, alpha=0.05):
+    '''
+    Return the bootstrap confidence interval for a point estimate.
+
+    Parameters:
+    ---------------
+    data: pd.DataFrame
+        A Dataframe containing summarizing statistics from multiple resamples. Typically it's the output of a `calculate` function.
+    interval: float
+        Significance level of the confidence interval. Example: 0.05 (default) represents the 95% confidence interval.
+
+    Returns:
+    ---------------
+    pd.DataFrame
+        Dataframe containing 1 row and columns for Statistic (Point Estimate), significance level, Lower Bound and Upper Bound.
     '''
     return pd.DataFrame()
--- pyfer/test/test_calculate.py
+++ pyfer/test/test_calculate.py
@@ -9,28 +9,25 @@ from calculate import calculate
 
 
 def test_calculate(calculate,df):
-  df = pd.read_csv("test_data_calculate.csv") 
+  df = pd.read_csv("test_data_calculate.csv")
   df_output = calculate(df)
 
   '''
   Test if the `calculate` function is working properly
-    
+
   Parameters:
-  `calculate`: 
+  `calculate`:
       The function being test
   df: dataFrame
       A Data frame using for test, which only contains 2 columns: sample_id, reponse
-        
+
   Returns:
-  string:  
+  string:
     "All tests pass. Success!" if all test are passed
     Raising error if some test is not passed.
   '''
     assert type(df_output) == "pandas.core.frame.DataFrame", "The type of the output is wrong"
-    assert df_output.shape == (10,2), "The shape of the output is wrong" 
+    assert df_output.shape == (10,2), "The shape of the output is wrong"
     assert type(df_output['mean'][0])== "numpy.int64", "The type of the output value is wrong"
     assert df_output['mean'][3] == 38.85, "The calculate mean is wrong"
-    print("All test passed") 
-
-
-
+    print("All test passed")
